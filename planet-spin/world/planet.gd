46e42b57b61839a34f19ef40176bcebe6ce33185
Jimbo Jones <128925976+jsteele15@users.noreply.github.com>
2026-06-22 10:35:35 +0100

    planet meshes
    - meshes for planets

--- planet-spin/world/planet.gd
+++ planet-spin/world/planet.gd
@@ -1,4 +1,21 @@
 class_name Planet extends Node3D
 
+
+@export var planet_mesh : MeshInstance3D
 @export var planets_sun : Planet
 @export var planet_name : int
+@export var planet_texture : CompressedTexture2D
+
+func _ready() -> void:
+	
+	match planet_name:
+		gameVars.PLANETS.SUN:
+			pass
+		gameVars.PLANETS.MERCURY:
+			pass
+		gameVars.PLANETS.VENUS:
+			pass
+		gameVars.PLANETS.EARTH:
+			pass
+		gameVars.PLANETS.MOON:
+			pass
